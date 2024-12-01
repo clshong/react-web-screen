9ba42a85020f604c8499c662e8df2c1ef84af754
charleshong <qcsjin8@gamil.com>
2024-12-01 11:40:58 +0800

    dev

---
+++ README.md
@@ -0,0 +1,4 @@
+### 项目初始化
+- pnpm create vite react-web-screen --template react-ts
+- pnpm i
+- react-router-dom 7.0.1
---
+++ src/router/index.tsx
@@ -0,0 +1,36 @@
+import {
+  createBrowserRouter,
+  Navigate,
+  type RouteObject,
+} from "react-router-dom";
+
+
+const routes: RouteObject[] = [
+  {
+    path: "/", // 根路径重定向
+    element: <Navigate to="/dashboard" replace />,
+  },
+  {
+    path: "/login",
+    lazy: () =>
+      import("@/views/Login").then((module) => ({ Component: module.default })),
+  },
+  {
+    path: "/dashboard",
+    lazy: () =>
+      import("@/views/Dashboard").then((module) => ({
+        Component: module.default,
+      })),
+  },
+];
+
+export const router = createBrowserRouter(routes, {
+  basename: import.meta.env.VITE_APP_BASE_URL,
+  future: {
+    v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  },
+});
